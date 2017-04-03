c68316a103be62fc6b8cb3e31f27b7532c687a48
Matt Cosand <matt@cosand.net>
2017-04-02 22:00:43 -0700

    log errors

--- email2sms/App_Start/WebApiConfig.cs
+++ email2sms/App_Start/WebApiConfig.cs
@@ -1,24 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Filters;
+using log4net;
 
 namespace email2sms
 {
-    public static class WebApiConfig
+  public static class WebApiConfig
+  {
+    public static void Register(HttpConfiguration config)
     {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
+      // Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApi",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional }
-            //);
-        }
+      // Web API routes
+      config.MapHttpAttributeRoutes();
+      config.Filters.Add(new ExceptionHandlingAttribute());
+      //config.Routes.MapHttpRoute(
+      //    name: "DefaultApi",
+      //    routeTemplate: "api/{controller}/{id}",
+      //    defaults: new { id = RouteParameter.Optional }
+      //);
     }
+  }
+
+  public class ExceptionHandlingAttribute : ExceptionFilterAttribute
+  {
+    public override void OnException(HttpActionExecutedContext context)
+    {
+      //Log Critical errors
+      LogManager.GetLogger("Errors").Error(context.Exception);
+
+      throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.InternalServerError)
+      {
+        Content = new StringContent("An error occurred, please try again or contact the administrator."),
+        ReasonPhrase = "Critical Exception"
+      });
+    }
+  }
 }
--- email2sms/Global.asax.cs
+++ email2sms/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
 using log4net.Config;
 
 namespace email2sms
@@ -17,6 +18,11 @@ namespace email2sms
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
       BundleConfig.RegisterBundles(BundleTable.Bundles);
+    }
+
+    protected void Application_Error()
+    {
+      LogManager.GetLogger("Errors").Error(Server.GetLastError());
     }
   }
 }
